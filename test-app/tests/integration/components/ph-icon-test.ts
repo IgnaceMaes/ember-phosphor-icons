2592926a0b1f2686e73b864e2015ee856dc9aedf
Ignace Maes <contact@ignacemaes.com>
2024-04-27 22:27:42 +0200

    test: add block coverage

--- test-app/tests/integration/components/ph-icon-test.ts
+++ test-app/tests/integration/components/ph-icon-test.ts
@@ -11,5 +11,31 @@ module('Integration | Component | ph-icon', function (hooks) {
 
     assert.dom().hasText('');
     assert.dom('[data-test-id="acorn"]').exists();
+    assert.dom('[data-test-id="acorn"]').hasTagName('svg');
+  });
+
+  test('it allows nesting animatiins', async function (assert) {
+    await render(hbs`
+    <PhAcorn data-test-id="acorn">
+      <animate
+        attributeName="opacity"
+        values="0;1;0"
+        dur="4s"
+        repeatCount="indefinite"
+        data-test-id="icon-animation"
+      />
+      <animateTransform
+        attributeName="transform"
+        attributeType="XML"
+        type="rotate"
+        dur="5s"
+        from="0 0 0"
+        to="360 0 0"
+        repeatCount="indefinite"
+      />
+    <PhAcorn />`);
+
+    assert.dom('[data-test-id="acorn"]').exists();
+    assert.dom('[data-test-id="icon-animation"]').exists();
   });
 });
