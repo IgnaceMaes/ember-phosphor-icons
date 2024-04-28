44b06fb1cf1291ab73fcafd80f428555c975b9fe
Ignace Maes <contact@ignacemaes.com>
2024-04-28 09:34:10 +0200

    refactor: extract base ph-icon class

---
+++ ember-phosphor-icons/src/ph-icon.ts
@@ -0,0 +1,41 @@
+import Component from '@glimmer/component';
+
+interface PhIconSignature {
+  Args: {
+    weight?: 'thin' | 'light' | 'regular' | 'bold' | 'fill' | 'duotone';
+    size?: string | number;
+    color?: string;
+    mirrored?: boolean;
+  };
+  Blocks: {
+    default: [];
+  };
+  Element: SVGElement;
+}
+
+export default class PhIcon extends Component<PhIconSignature> {
+  get weight() {
+    return this.args.weight ?? 'regular';
+  }
+
+  get size() {
+    return this.args.size ?? '1em';
+  }
+
+  get color() {
+    return this.args.color ?? 'currentColor';
+  }
+
+  get mirrored() {
+    const contextMirrored = this.args.mirrored ?? false;
+    return this.args.mirrored !== undefined
+      ? this.args.mirrored
+        ? 'scale(-1, 1)'
+        : undefined
+      : contextMirrored
+        ? 'scale(-1, 1)'
+        : undefined;
+  }
+
+  eq = (a: string, b: string) => a === b;
+}
