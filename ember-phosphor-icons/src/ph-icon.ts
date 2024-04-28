db3a022a7d3ded866a9930c0d7c10772431744ea
Ignace Maes <contact@ignacemaes.com>
2024-04-28 14:10:04 +0200

    docs: add jsdoc arg info

--- ember-phosphor-icons/src/ph-icon.ts
+++ ember-phosphor-icons/src/ph-icon.ts
@@ -2,9 +2,21 @@ import Component from '@glimmer/component';
 
 interface PhIconSignature {
   Args: {
+    /**
+     * Icon weight/style. Can also be used, for example, to "toggle" an icon's state: a rating component.
+     */
     weight?: 'thin' | 'light' | 'regular' | 'bold' | 'fill' | 'duotone';
+    /**
+     * Icon height & width. As with standard React elements, this can be a number, or a string with units in `px`, `%`, `em`, `rem`, `pt`, `cm`, `mm`, `in`.
+     */
     size?: string | number;
+    /**
+     * Icon stroke/fill color. Can be any CSS color string, including `hex`, `rgb`, `rgba`, `hsl`, `hsla`, named colors, or the special `currentColor` variable.
+     */
     color?: string;
+    /**
+     * Flip the icon horizontally. Can be useful in RTL languages where normal icon orientation is not appropriate.
+     */
     mirrored?: boolean;
   };
   Blocks: {
